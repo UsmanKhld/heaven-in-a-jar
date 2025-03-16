0623b3bdc72f588eac562148e223f2299232524e
Usman Khalid <usmankhld602@gmail.com>
2025-03-16 01:45:19 -0500

    this stuff

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,14 @@ import { initializeApp } from "firebase/app";
 import { getDatabase, ref, onValue } from "firebase/database";
 
 const firebaseConfig = {
-	apiKey: "AIzaSyAtvuRR_znNr6yvTA9JMhEuxzoqLHlY3vE",
-	authDomain: "heaven-in-a-jar.firebaseapp.com",
-	databaseURL: "https://heaven-in-a-jar-default-rtdb.firebaseio.com",
-	projectId: "heaven-in-a-jar",
-	storageBucket: "heaven-in-a-jar.firebasestorage.app",
-	messagingSenderId: "306165318483",
-	appId: "1:306165318483:web:e33ed784fe9722efc2d7a6",
-	measurementId: "G-Y73N89H56W",
+	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+	databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+	appId: process.env.REACT_APP_FIREBASE_APP_ID,
+	measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
