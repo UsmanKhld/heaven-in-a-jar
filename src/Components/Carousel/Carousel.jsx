8cd327cc7aeedc876a3c2590c8ed9eb420096fbf
Usman Khalid <usmankhld602@gmail.com>
2025-03-21 16:18:23 -0500

    more carousel but broken css

--- src/Components/Carousel/Carousel.jsx
+++ src/Components/Carousel/Carousel.jsx
@@ -1,13 +1,36 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { database, ref, onValue } from "../../firebase";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination } from "swiper/modules";
+import "./Carousel.css";
 
 import "swiper/css";
 import "swiper/css/pagination";
 
 const Carousel = () => {
   const [carouselData, setCarouselData] = useState({});
+  const swiperWrappedRef = useRef(null);
+
+  function adjustMargin() {
+    const screenWidth = window.innerWidth;
+
+    if (swiperWrappedRef.current) {
+      swiperWrappedRef.current.style.marginLeft =
+        screenWidth <= 520
+          ? "0px"
+          : screenWidth <= 650
+          ? "-50px"
+          : screenWidth <= 800
+          ? "-100px"
+          : "-150px";
+    }
+  }
+
+  useEffect(() => {
+    adjustMargin();
+    window.addEventListener("resize", adjustMargin);
+    return () => window.removeEventListener("resize", adjustMargin);
+  }, []);
 
   useEffect(() => {
     const carouselRef = ref(database, "carousel");
@@ -18,14 +41,40 @@ const Carousel = () => {
   }, []);
 
   return (
-    <div className="carousel_container">
-      <Swiper modules={[Pagination]}>
-        {carouselData.map((slide, index) => (
-          <SwiperSlide key={index}>
-            <img src={slide.imageUrl} alt={slide.title} />
-          </SwiperSlide>
-        ))}
-      </Swiper>
+    <div className="component_container">
+      <div className="carousel_container">
+        <Swiper
+          modules={[Pagination]}
+          grabCursor
+          initialSlide={2}
+          centeredSlides
+          slidesPerView={"auto"}
+          speed={800}
+          slideToClickedSlide
+          pagination={{ clickable: true }}
+          breakpoints={{
+            320: { spaceBetween: 40 },
+            650: { spaceBetween: 30 },
+            1000: { spaceBetween: 20 },
+          }}
+          onSwiper={(swiper) => {
+            swiperWrappedRef.current = swiper.wrapperEl;
+          }}
+        >
+          {Object.keys(carouselData).map((key, index) => (
+            <SwiperSlide key={index}>
+              <img
+                className="car_image"
+                src={carouselData[key].imageUrl}
+                alt={carouselData[key].title}
+              />
+              <div className="carousel_title">
+                <h1>{carouselData[key].title}</h1>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
     </div>
   );
 };
