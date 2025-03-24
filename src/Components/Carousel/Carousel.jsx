28e599101dd23826376d8610b1ccbd5b6942859d
Usman Khalid <usmankhld602@gmail.com>
2025-03-24 16:34:07 -0500

    did home about, will redo carousel

--- src/Components/Carousel/Carousel.jsx
+++ src/Components/Carousel/Carousel.jsx
@@ -1,36 +1,9 @@
 import React, { useEffect, useState, useRef } from "react";
 import { database, ref, onValue } from "../../firebase";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination } from "swiper/modules";
+
 import "./Carousel.css";
-
-import "swiper/css";
-import "swiper/css/pagination";
-
 const Carousel = () => {
   const [carouselData, setCarouselData] = useState({});
-  const swiperWrappedRef = useRef(null);
-
-  function adjustMargin() {
-    const screenWidth = window.innerWidth;
-
-    if (swiperWrappedRef.current) {
-      swiperWrappedRef.current.style.marginLeft =
-        screenWidth <= 520
-          ? "0px"
-          : screenWidth <= 650
-          ? "-50px"
-          : screenWidth <= 800
-          ? "-100px"
-          : "-150px";
-    }
-  }
-
-  useEffect(() => {
-    adjustMargin();
-    window.addEventListener("resize", adjustMargin);
-    return () => window.removeEventListener("resize", adjustMargin);
-  }, []);
 
   useEffect(() => {
     const carouselRef = ref(database, "carousel");
@@ -42,39 +15,7 @@ const Carousel = () => {
 
   return (
     <div className="component_container">
-      <div className="carousel_container">
-        <Swiper
-          modules={[Pagination]}
-          grabCursor
-          initialSlide={2}
-          centeredSlides
-          slidesPerView={"auto"}
-          speed={800}
-          slideToClickedSlide
-          pagination={{ clickable: true }}
-          breakpoints={{
-            320: { spaceBetween: 40 },
-            650: { spaceBetween: 30 },
-            1000: { spaceBetween: 20 },
-          }}
-          onSwiper={(swiper) => {
-            swiperWrappedRef.current = swiper.wrapperEl;
-          }}
-        >
-          {Object.keys(carouselData).map((key, index) => (
-            <SwiperSlide key={index}>
-              <img
-                className="car_image"
-                src={carouselData[key].imageUrl}
-                alt={carouselData[key].title}
-              />
-              <div className="carousel_title">
-                <h1>{carouselData[key].title}</h1>
-              </div>
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      </div>
+      <h1>placeholder</h1>
     </div>
   );
 };
--- src/Components/HomeAbout/HomeAbout.jsx
+++ src/Components/HomeAbout/HomeAbout.jsx
@@ -1,10 +1,30 @@
 import React from "react";
 import "./homeAbout.css";
+import store from "../../assets/Store.jpg";
 
 export const HomeAbout = () => {
   return (
     <div className="ha_container">
-      <h1>PlaceHolder</h1>
+      <div className="ha_background">
+        <div className="ha_content">
+          <h1>Heaven In A Jar</h1>
+          <p>Your one stop shop to satisfy all your sweet cravings</p>
+          {/* Add more content/information here */}
+          <div className="ha_info">
+            <a
+              href="https://www.google.com/maps/place/Heaven+In+a+Jar,+Desserts/@29.5933171,-95.6065593,20.25z/data=!4m6!3m5!1s0x8640e689a88b50d7:0x8e6660d5b8afacaa!8m2!3d29.5934001!4d-95.6062062!16s%2Fg%2F11f55nl3fn?entry=ttu&g_ep=EgoyMDI1MDMxOS4yIKXMDSoJLDEwMjExNDU1SAFQAw%3D%3D"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <p className="hover:text-blue-400 hover:underline transition-all">
+                3159 B Hwy 6 Sugar Land, TX
+              </p>
+            </a>
+            <p>Made with love, care, and attention to detail</p>
+            <p>Your local, family owned business since 2018</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
