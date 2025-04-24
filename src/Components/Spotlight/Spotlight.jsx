04b0f223563fb546613cc8cd43325fd2cddc1ae0
Usman Khalid <usmankhld602@gmail.com>
2025-04-23 20:54:34 -0500

    fixed carousel, navbar logo, homeabout

--- src/Components/Spotlight/Spotlight.jsx
+++ src/Components/Spotlight/Spotlight.jsx
@@ -6,43 +6,41 @@ import "swiper/css/pagination";
 import { Navigation, Pagination, Autoplay } from "swiper/modules";
 import "./Spotlight.css";
 
-import dubaiChoc from "../../assets/DubaiChocSpotlight.png";
-import dessert1 from "../../assets/gift-box.jpg";
-import dessert2 from "../../assets/gift-bag.jpg";
+import dubaiChoc from "../../assets/carousel/DubaiChocDel.png";
+import dubaiStrawberry from "../../assets/carousel/DubaiChocolateStrawberries.png";
 
 const Spotlight = () => {
-  const images = [
-    { src: dubaiChoc, alt: "Dubai Chocolate Spotlight" },
-    { src: dessert1, alt: "Delicious Dessert 1" },
-    { src: dessert2, alt: "Delicious Dessert 2" },
-  ];
+	const images = [
+		{ src: dubaiChoc, alt: "Dubai Chocolate Spotlight" },
+		{ src: dubaiStrawberry, alt: "Delicious Dessert 1" },
+	];
 
-  return (
-    <div className="spotlight-container">
-      <div className="spotlight-content">
-        <Swiper
-          modules={[Navigation, Pagination, Autoplay]}
-          navigation
-          pagination={{ clickable: true }}
-          autoplay={{ delay: 3000 }}
-          loop={true}
-          className="spotlight-carousel"
-        >
-          {images.map((image, index) => (
-            <SwiperSlide key={index}>
-              <div className="spotlight-image-container">
-                <img
-                  src={image.src}
-                  alt={image.alt}
-                  className="spotlight-image"
-                />
-              </div>
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      </div>
-    </div>
-  );
+	return (
+		<div className="spotlight-container">
+			<div className="spotlight-content">
+				<Swiper
+					modules={[Navigation, Pagination, Autoplay]}
+					navigation
+					pagination={{ clickable: true }}
+					autoplay={{ delay: 5000 }}
+					loop={true}
+					className="spotlight-carousel"
+				>
+					{images.map((image, index) => (
+						<SwiperSlide key={index}>
+							<div className="spotlight-image-container">
+								<img
+									src={image.src}
+									alt={image.alt}
+									className="spotlight-image"
+								/>
+							</div>
+						</SwiperSlide>
+					))}
+				</Swiper>
+			</div>
+		</div>
+	);
 };
 
 export default Spotlight;
