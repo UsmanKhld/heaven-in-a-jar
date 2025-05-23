68a243616c38eaf3cc3ba3cf27aafe542753d5f6
Usman Khalid <usmankhld602@gmail.com>
2025-05-23 16:18:27 -0500

    tryiung analytics tag

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Analytics } from "@vercel/analytics/react";
 import "./index.css";
 import App from "./App.jsx";
 import "leaflet/dist/leaflet.css";
@@ -7,5 +8,6 @@ import "leaflet/dist/leaflet.css";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>
 );
